b39acc4c436f3f646d1a398ebe8dea3376dcc8a7
Nick Chan <nickharuchan@gmail.com>
2025-10-01 10:18:41 -0400

    added methods to click and drag the ball, and to change rigidbody type from dynamic to kinematic to pull up tension

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -4,19 +4,48 @@ using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
     public Camera mainCamera;
+    public SpringJoint2D currentBallSpringJoint;
+    public float delayTime = 5f;
+    private bool isDragging;
+    public Rigidbody2D currentBallRigidbody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!Touchscreen.current.primaryTouch.press.isPressed) return;
+        if (currentBallRigidbody == null) return;
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            if (isDragging)
+            {
+                LaunchBall();
+            }
+            
+            isDragging = false;
+            return;
+        }
+
+        isDragging = true;
+        currentBallRigidbody.bodyType = RigidbodyType2D.Kinematic;
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
         
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
-        Debug.Log(worldPosition);
+        currentBallRigidbody.position = worldPosition;
+    }
+
+    private void LaunchBall()
+    {
+        currentBallRigidbody.bodyType = RigidbodyType2D.Dynamic;
+        currentBallRigidbody = null;
+        Invoke(nameof(DetachBall), delayTime);
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
     }
 }
