3ae4963ca595fd8987072c69e234bad16f7e0bb2
Nick Chan <nickharuchan@gmail.com>
2025-10-02 01:42:57 -0400

    Added Ball Respawning system, and Cinemachine system where it keeps track of 2 points to make sure it is always displayed making it playable on any device size

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class BallHandler : MonoBehaviour
 {
+    public GameObject ballPrefab;
+    public Rigidbody2D pivot;
     public Camera mainCamera;
-    public SpringJoint2D currentBallSpringJoint;
-    public float delayTime = 5f;
+    public float delayTime;
+    public float respawnTime;
     private bool isDragging;
-    public Rigidbody2D currentBallRigidbody;
+    private SpringJoint2D currentBallSpringJoint;
+    private Rigidbody2D currentBallRigidbody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        SpawnNewBall();
     }
 
     // Update is called once per frame
@@ -36,11 +41,22 @@ public class BallHandler : MonoBehaviour
         currentBallRigidbody.position = worldPosition;
     }
 
+    private void SpawnNewBall()
+    {
+        GameObject ballInstance = Instantiate(ballPrefab, pivot.position, Quaternion.identity);
+        
+        currentBallRigidbody = ballInstance.GetComponent<Rigidbody2D>();
+        currentBallSpringJoint = ballInstance.GetComponent<SpringJoint2D>();
+        
+        currentBallSpringJoint.connectedBody = pivot;
+    }
+
     private void LaunchBall()
     {
         currentBallRigidbody.bodyType = RigidbodyType2D.Dynamic;
         currentBallRigidbody = null;
         Invoke(nameof(DetachBall), delayTime);
+        Invoke(nameof(SpawnNewBall), respawnTime);
     }
 
     private void DetachBall()
